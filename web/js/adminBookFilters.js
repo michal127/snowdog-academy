e038f0aea42584f7c9b8d0f5aa181d6fadf4ae71
Michał Daniel <mic127@onet.eu>
2020-07-19 16:50:01 +0200

    Refactoring - removed window.onload

--- web/js/adminBookFilters.js
+++ web/js/adminBookFilters.js
@@ -1,25 +1,23 @@
-window.onload = event => {
-  const
-    bookFiltersForm = document.querySelector('#bookFilters'),
-    minBorrowDaysInput = bookFiltersForm.querySelector('#minBorrowDay'),
-    bookFilterSubmit = bookFiltersForm.querySelector('#bookFilterSubmit'),
-    bookFilterReset = bookFiltersForm.querySelector('#bookFilterReset')
+const
+  bookFiltersForm = document.querySelector('#bookFilters'),
+  minBorrowDaysInput = bookFiltersForm.querySelector('#minBorrowDay'),
+  bookFilterSubmit = bookFiltersForm.querySelector('#bookFilterSubmit'),
+  bookFilterReset = bookFiltersForm.querySelector('#bookFilterReset')
 
-  bookFiltersForm.addEventListener('submit', onFormSubmit)
-  minBorrowDaysInput.addEventListener('input', onFilterChange)
+bookFiltersForm.addEventListener('submit', onFormSubmit)
+minBorrowDaysInput.addEventListener('input', onFilterChange)
 
-  function onFormSubmit (e) {
-    e.preventDefault()
-    const isResetForm = (e.submitter === bookFilterReset)
-    let targetLocation = '/admin'
-    if (!isResetForm && minBorrowDaysInput.value > 0) {
-      targetLocation += '/books/minDays/' + minBorrowDaysInput.value
-    }
-
-    window.location.href = targetLocation
+function onFormSubmit (e) {
+  e.preventDefault()
+  const isResetForm = (e.submitter === bookFilterReset)
+  let targetLocation = '/admin'
+  if (!isResetForm && minBorrowDaysInput.value > 0) {
+    targetLocation += '/books/minDays/' + minBorrowDaysInput.value
   }
 
-  function onFilterChange () {
-    bookFilterSubmit.toggleAttribute('disabled', false)
-  }
+  window.location.href = targetLocation
+}
+
+function onFilterChange () {
+  bookFilterSubmit.toggleAttribute('disabled', false)
 }
